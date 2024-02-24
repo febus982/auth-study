54df2eda36791e360a14e76348b355d58e92b665
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-02-24 14:35:36 +0000

    Make hello world route protected

--- src/http_app/routes/hello.py
+++ src/http_app/routes/hello.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter, Request
+import logging
+from typing import Annotated
+
+from joserfc import jwt, jwk
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from httpx import AsyncClient
 
 from http_app.templates import templates
 
@@ -7,5 +13,21 @@ router = APIRouter(prefix="/hello")
 
 
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
-async def hello(request: Request):
+async def hello(
+    request: Request,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())]
+):
+    async with AsyncClient() as ac:
+        response = await ac.get("http://oathkeeper:4456/.well-known/jwks.json")
+        response.raise_for_status()
+
+    jwks = jwk.KeySet.import_key_set(response.json())
+
+    token = jwt.decode(
+        value=credentials.credentials,
+        key=jwks,
+    )
+    logging.info(token.header)
+    logging.info(token.claims)
+
     return templates.TemplateResponse("hello.html", {"request": request})
