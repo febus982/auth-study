ab61367db5c53dbbf672979e74290e0cff98a2c2
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-02-24 15:21:55 +0000

    Multiple api endpoints

--- src/http_app/routes/hello.py
+++ src/http_app/routes/hello.py
@@ -11,23 +11,51 @@ from http_app.templates import templates
 
 router = APIRouter(prefix="/hello")
 
-
-@router.get("/", response_class=HTMLResponse, include_in_schema=False)
-async def hello(
-    request: Request,
-    credentials: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())]
-):
+# Not part of this PoC, we should use a TTL cache here (i.e. 1H)
+async def get_jwks() -> jwk.KeySet:
     async with AsyncClient() as ac:
         response = await ac.get("http://oathkeeper:4456/.well-known/jwks.json")
         response.raise_for_status()
 
-    jwks = jwk.KeySet.import_key_set(response.json())
+    return jwk.KeySet.import_key_set(response.json())
 
-    token = jwt.decode(
+
+async def get_token(
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())],
+    jwks: Annotated[jwk.KeySet, Depends(get_jwks)],
+) -> jwt.Token:
+    return jwt.decode(
         value=credentials.credentials,
         key=jwks,
     )
-    logging.info(token.header)
-    logging.info(token.claims)
+
+
+@router.get("/", response_class=HTMLResponse, include_in_schema=False)
+async def hello(
+    request: Request,
+    jwt_token: Annotated[jwt.Token, Depends(get_token)]
+):
+    logging.info(jwt_token.header)
+    logging.info(jwt_token.claims)
+
+    async with AsyncClient() as ac:
+        response = await ac.get(
+            "http://127.0.0.1:8000/hello/new",
+            headers={"Authorization": request.headers["Authorization"]}
+        )
+        response.raise_for_status()
+
+    return templates.TemplateResponse("hello.html", {"request": request})
+
+
+@router.get("/new", response_class=HTMLResponse, include_in_schema=False)
+async def hello_new(
+    request: Request,
+    jwt_token: Annotated[jwt.Token, Depends(get_token)]
+):
+
+    logging.info("CALLED NEW")
+    logging.info(jwt_token.header)
+    logging.info(jwt_token.claims)
 
     return templates.TemplateResponse("hello.html", {"request": request})
